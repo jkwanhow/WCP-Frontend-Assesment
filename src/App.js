29f4c2b5cb680ec8cf9e9b574ff5cb5c6f292474
jkwanhow <jeffery0kwan0how@gmail.com>
2022-08-21 03:56:29 +1000

    Changed tab name and icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import "./App.css";
 import Container from './components/container';
 
 function App() {
+  useEffect(() => {
+    document.title = "Robot Cards";
+  }, []);
   return (
     <div>
       <Container />
